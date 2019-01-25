b3d262a5032f484e8e19ed5850c078de66e70016
“z” <u6015364@anu.edu.au>
2019-01-25 15:08:27 +0800

    Calculate recall precision

--- Src/test/Calculate_precision_recall.py
+++ Src/test/Calculate_precision_recall.py
@@ -76,6 +76,6 @@ for result in tqdm.tqdm(test_results):
 print(np.sum(IoU)/len(IoU))
 print(np.sum(recall)/len(recall))
 print(np.sum(precision)/len(precision))
-print(diff_threshold)
+print(threshold)
 
 np.save("precesion.npy", (IoU, recall, precision))
