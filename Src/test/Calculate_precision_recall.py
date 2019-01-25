d4e781f25ae54f4acac88561467d5d14258916a6
“z” <u6015364@anu.edu.au>
2019-01-25 14:30:55 +0800

    Calculate recall precision

--- Src/test/Calculate_precision_recall.py
+++ Src/test/Calculate_precision_recall.py
@@ -21,10 +21,20 @@ for result in tqdm.tqdm(test_results):
     # Calucuate diff threshold
     m_list = delta.flatten()
     h_dic = dict(Counter(m_list))
+#     for i in range(max(h_dic.keys()),-1,-1):
+#         if i in h_dic.keys() and h_dic[i] > 1200: # 100, 500, 1000
+#             threshold = i
+#             break
+            
+    pixel_sum = 0
+    threshold = 0
+    mask_num = original_mask.size
     for i in range(max(h_dic.keys()),-1,-1):
-        if i in h_dic.keys() and h_dic[i] > 1200: # 100, 500, 1000
-            threshold = i
-            break
+        if i in h_dic.keys():
+            pixel_sum += h_dic[i]
+            if pixel_sum > mask_num:
+                threshold = i
+                break
     
     # Get original mask
     original_mask = np.zeros((delta.shape[1], delta.shape[0]))
