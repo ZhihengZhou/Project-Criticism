e78c25aabc3511666e7428ae175f9c79435a2f0f
“z” <u6015364@anu.edu.au>
2019-01-30 12:23:22 +0800

    Calculate precision recall IoU

--- Src/test/Calculate_precision_recall.py
+++ Src/test/Calculate_precision_recall.py
@@ -36,7 +36,7 @@ for result in tqdm.tqdm(test_results):
 #             if pixel_sum > mask_num:
 #                 threshold = i
 #                 break
-    threshold = 70
+    threshold = 80
     
     # Get original mask
     original_mask = np.zeros((delta.shape[1], delta.shape[0]))
